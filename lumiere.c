5a88b5bdd0ad9e27c1f507f4a87d645c479c6598
vincent <vincent@localhost.localdomain>
2019-12-06 03:01:54 -0800

    test

--- lumiere.c
+++ lumiere.c
@@ -2,9 +2,6 @@
 
 void afficherFete(){
 
-<<<<<<< HEAD
+
 	printf("La fête  des lumières commence demain \n");
-=======
-	printf("La    cdccccc  fête des lumières commence demain \n");
->>>>>>> fe9cb9b4b73c2b9b0e5d419d15c6e9b05b3b0727
 }
